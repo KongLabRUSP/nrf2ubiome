2a426d6c9af409d39937e51627b94724a0d987a4
Administrator <administrator@SOP-1482>
2018-11-28 14:56:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+##  Project: Nrf2 BL6 PEITC 16S microbiome data analysis
+### Study ID: 
+### Scientist: Ran Yin, Renyi Wu
+### Data Analysis: Davit Sargsyan 
+### Created: 11/28/2018
+
+---    
+
+## Table of Contents
+[Daily Logs](#logs)  
+[Results](#results)   
+[References](#ref)   
+[Files](#files)
+
+## Daily Logs<a name="logs"></a>
+### 11/28/2018
+* 16S FastQ files downloaded to:    
+`/datastorage/FastQ_2018/16s/November`
---
+++ source/nrf2ubiome_data_v1.R
@@ -0,0 +1,13 @@
+# |----------------------------------------------------------------------------------|
+# | Project: Nrf2 BL6 PEITC 16S microbiome data analysis                             |
+# | Script: Data                                                                     |
+# | Coordinator: Ran Yin, Renyi Wu                                                   |
+# | Author: Davit Sargsyan                                                           |
+# | Created: 11/28/2018                                                              |
+# | Modified:                                                                        |
+# |----------------------------------------------------------------------------------|
+# sink(file = "tmp/log_skin_uvb_dna_v2.txt")
+date()
+
+# sessionInfo()
+# sink()
