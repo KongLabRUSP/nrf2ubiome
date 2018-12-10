e333f5531242e3fc75ef0a7c8b13fd709ef00a3e
Davit Sargsyan <sargdavid@gmail.com>
2018-12-10 08:58:16 -0500

    Continue DADA2

--- source/nrf2ubiome_dada2_v1.R
+++ source/nrf2ubiome_dada2_v1.R
@@ -25,6 +25,8 @@ require(data.table)
 require(dada2)
 packageVersion("dada2")
 # [1] ‘1.10.0’
+require(phyloseq)
+require(ggplot2)
 
 path <- "fastq"
 list.files(path = path,
@@ -318,19 +320,79 @@ save(dadaRs,
      file = "data/dadaRs.RData")
 
 # Merge paired reads----
-mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose=TRUE)
+# load("data/dadaFs.RData")
+# load("data/dadaRs.RData")
+
+mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose = TRUE)
+save(mergers,
+     file = "data/mergers.RData")
+
+# load("data/mergers.RData")
 # Inspect the merger data.frame from the first sample
 head(mergers[[1]])
 
 # Construct sequence table
 seqtab <- makeSequenceTable(mergers)
-#seqtab <- makeSequenceTable(dadaFs)
-head(dadaFs)
 dim(seqtab)
 
 # Remove chimeras----
-seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE, verbose=TRUE)
+seqtab.nochim <- removeBimeraDenovo(seqtab, 
+                                    method = "consensus",
+                                    multithread = FALSE, 
+                                    verbose = TRUE)
 dim(seqtab.nochim)
+save(seqtab.nochim,
+     file = "data/seqtab.nochim.RData")
+
+# Assign taxonomy----
+taxa <- assignTaxonomy(seqs = seqtab.nochim,
+                       refFasta = "~/tax/silva_nr_v128_train_set.fa.gz",
+                       multithread = TRUE)
+dim(taxa)
+View(head(taxa))
+save(taxa,
+     file = "data/taxa.RData")
+
+# # SIDE ANALYSIS: save taxa obtained before removeing bimeras----
+# taxa_with_bimera <- taxa
+# save(taxa_with_bimera,
+#      file = "data/taxa_with_bimera.RData")
+
+# Removing sequence rownames for display only----
+taxa.print <- taxa 
+rownames(taxa.print) <- NULL
+head(taxa.print)
+
+# Handoff to phyloseq----
+samples.out <- rownames(seqtab.nochim)
+subject <- sapply(strsplit(samples.out, "D"), `[`, 1)
+gender <- substr(subject,1,1)
+subject <- substr(subject,2,999)
+day <- as.integer(sapply(strsplit(samples.out, "D"), `[`, 2))
+samdf <- data.frame(Subject=subject, Gender=gender, Day=day)
+samdf$When <- "Early"
+samdf$When[samdf$Day>100] <- "Late"
+rownames(samdf) <- samples.out
+
+ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
+               sample_data(samdf), 
+               tax_table(taxa))
+ps <- prune_samples(sample_names(ps) != "Mock", ps) # Remove mock sample
+ps
+
+plot_richness(ps, x="Day", measures=c("Shannon", "Simpson"), color="When")
+
+# Transform data to proportions as appropriate for Bray-Curtis distances
+ps.prop <- transform_sample_counts(ps, function(otu) otu/sum(otu))
+ord.nmds.bray <- ordinate(ps.prop, method="NMDS", distance="bray")
+
+plot_ordination(ps.prop, ord.nmds.bray, color="When", title="Bray NMDS")
+
+# Barplots
+top20 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:20]
+ps.top20 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
+ps.top20 <- prune_taxa(top20, ps.top20)
+plot_bar(ps.top20, x="Day", fill="Family") + facet_wrap(~When, scales="free_x")
 
 #sessionInfo()
 # sink()
